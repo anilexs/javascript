ba888d08dacca95726269144aa6cb1636ce573dc
alexis <alexissimoes94@gmail.com>
2023-05-23 14:32:28 +0200

    js 1 base 23/05/23

---
+++ exercise/js/exercice.js
@@ -0,0 +1,15 @@
+var Prenom = "Prenom"; // string
+
+var monNumero = 65; // numerique / number
+
+var monNum = "100";
+
+//var nom = izadi; //  syntaxe false    //false ( sais pas posible car sais une chaine de caracter mais il menque les gimet)
+
+var booleanFalse = false; // boolean
+
+var booleanTrue = true; // boolean
+
+var varNull = null; // null
+
+console.log(Prenom);
