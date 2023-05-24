cc3cabfc7326ed80698b8c376dbf305d136d6575
alexis <alexissimoes94@gmail.com>
2023-05-24 16:57:00 +0200

    ex 24/05/23

--- exercise/js/exercice.js
+++ exercise/js/exercice.js
@@ -1,15 +1,63 @@
-var Prenom = "Prenom"; // string
+// var Prenom = "alexis"; // string var monNumero = 65; // numerique / number
 
-var monNumero = 65; // numerique / number
-
-var monNum = "100";
+// var monNum = "100";
 
 //var nom = izadi; //  syntaxe false    //false ( sais pas posible car sais une chaine de caracter mais il menque les gimet)
 
-var booleanFalse = false; // boolean
+// var booleanFalse = false; // boolean
 
-var booleanTrue = true; // boolean
+// var booleanTrue = true; // boolean
 
-var varNull = null; // null
+// var varNull = null; // null
 
-console.log(Prenom);
+// console.log(Prenom);
+// Afficher une boîte de dialogue (2 façons)
+// alert(Prenom); // lie la variable en boite de dialog
+// alert("voici mon \(Prenom) prenom mitra"); // lie la string en boite de dialog
+// window.alert("Super, tu es arrivé sur mon site !");
+// document.write("A la pause vous aurez des Haribos Pik !"); // Afficher dans la page web
+// -- 7 : Demander à l'utilisateur une valeur (2 façons)
+// window.prompt("Question: on est quel jour ?", "jour de la semaine");
+// prompt("Question: on est quel jour ?", "jour de la semaine");
+// -- et pour manipuler cette valeur, je n'oublie pas de la stocker
+// var jour = prompt("Question: on est quel jour ?", );
+// pour apeller la variable jour dant ma consol
+// console.log(jour);
+// pour aficher le resulta sur ma page web
+// document.write(jour);
+
+// -- 2. Un nombre entier (integer ou int)
+// var annee = 2017;
+
+// -- 4. Un booléen (boolean = VRAI/FAUX = TRUE/FALSE)
+// var unBooleen = false; 
+// unBooleen = true; 
+// console.log(unBooleen);
+// const PAYS = "france";
+// PAYS = "Italie"; // on ne pe pas modifier une const ( constence )
+// document.write(PAYS);
+
+// STRING => NUMBER
+// var unChiffre = "128.5698";
+// unChiffre = parseFloat(unChiffre);
+// console.log(unChiffre);
+// unChiffre = parseInt(unChiffre);
+// console.log(unChiffre);
+
+//  metre un number en string
+// var namNumber = 150;
+// namNumber = namNumber.toString();
+// console.log(namNumber);
+
+// var prenom = "alexis";
+// var nom = "simoes";
+var prenom = prompt("quelle et votre prenom ?", "alexis");
+console.log(prenom);
+document.write(prenom);
+alert(prenom);
+
+// Differents types de fonctions : (function)
+//  1 - une fonction avec au moins un parametre sans valeur de retour
+//  2 - une fonction avec au moins un parametre avec une valeur de retour
+//  3 - une fonction sans  parametre avec une valeur de retour
+//  4 - une fonction sans  parametre sans valeur de retour
