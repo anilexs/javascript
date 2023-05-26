cec7731be8d83fc4100598098336b06facbe66e7
alexis <alexissimoes94@gmail.com>
2023-05-26 16:54:20 +0200

    26/05/23 java

---
+++ initiation/!ex_perso/js/ex1.js
@@ -0,0 +1,8 @@
+var nb1 = 10;
+if (nb1 < 50){ // si "if" est true :
+    console.log("nb1 est bien inferieur a 50 car jais comme nuber "+ nb1 );
+}else if (nb1 == 50) { // si "if n'est pas true et que "else if" n'est pas true ;
+    console.log("Je suis dant else if");
+}else{ // sinon, si "if" n'est pas true et que "else if" n'est pas true non plus :
+    console.log("Je suis dant else");
+}
