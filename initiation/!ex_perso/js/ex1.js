a75f531e1150c6f77137fb1d9a92260fe8c3898c
alexis <alexissimoes94@gmail.com>
2023-05-31 16:19:36 +0200

    31/05/23

--- initiation/!ex_perso/js/ex1.js
+++ initiation/!ex_perso/js/ex1.js
@@ -33,17 +33,78 @@
 // document.write("<h3>Il aura fallu " + tps + " mois.</h3>");
 
 
-var monCompte = 1000;
-var temps = 0;
-while(monCompte < 2000){
-    monCompte += 50;
-    temps++;
-}
-document.write("<h3>Sous sur mon compte : " + monCompte + "</h3>");
-document.write("<h3>Jusqu'à 2000€, il me faut : " + temps + " mois</h3>");
+// var monCompte = 1000;
+// var temps = 0;
+// while(monCompte < 2000){
+//     monCompte += 50;
+//     temps++;
+// }
+// document.write("<h3>Sous sur mon compte : " + monCompte + "</h3>");
+// document.write("<h3>Jusqu'à 2000€, il me faut : " + temps + " mois</h3>");
 
-for(var mesSous = 100; mesSous < 2000; mesSous+=50){
-    tps++;
+// for(var mesSous = 100; mesSous < 2000; mesSous+=50){
+//     tps++;
+// }
+// document.write("<h3>Le mois suivant j'ai : " + mesSous + "</h3>");
+// document.write("<h3>Il aura fallu " + tps + " mois.</h3>");
+
+
+// var fruits = ["Apple", "Banana"];
+// var fruits2 = {
+//   0: 'Apple',
+//   1: 'Banana',
+//   'troisiemeFruit': 'orange'
+// };
+// var prenom = ["Nawal","Wassila","Rahim","Jérémy","Narcis","Karima","Faissal","Alin","Oliver","Cynthia","Alexis","Michel","Mitra",];
+// console.log(fruits.length);
+// // le nombre de valeur dant le tableau
+// console.log(prenom.length);
+// // Acceder(via son index) a un element du tableau
+// var first = fruits[0];
+// var second = fruits[0];
+// var third = fruits2['troisiemeFruit'];
+// console.log(second);
+// console.log("La valeur du troisième élément : " + third);
+
+
+// var fruits = ['Apple', 'Banana'];
+// var prenom = ['Nawal', 'Wassila', 'Rahim', 'Jérémy', 'Narcis', 'Karima', 'Faïssal', 'Alin', 'Oliver', 'Cynthia', 'Alexis', 'Michel', 'Mitra'];
+// console.log(fruits.length);
+// // 2
+// console.log(prenom.length);
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var fruits2 = {
+//     0: 'Apple',
+//     1: 'Banana',
+//     'troisemeFruit': 'orange'
+// }
+
+// var prenom = ['Nawal', 'Wassila', 'Rahim', 'Jérémy', 'Narcis', 'Karima', 'Faïssal', 'Alin', 'Oliver', 'Cynthia', 'Alexis', 'Michel', 'Mitra'];
+// console.log(fruits.length);
+// // 3
+// console.log(prenom.length);
+// //Accéder(via son index) à un élément du tableau
+// var first = fruits[0];
+// var second = fruits2[0];
+// var third = fruits2['troisemeFruit'];
+// console.log(second);
+// console.log("La valeur du troisième élément :" +
+//     third);
+
+// var couleurs = ['rouge', 'vert', 'bleu'];
+// for (var i = 0; i < couleurs.length; i++) {
+//     console.log(couleurs[i]);
+// }
+
+var couleurs = ['rouge', 'vert', 'bleu'];
+for (var w = 0; w < couleurs.length; w++) {
+    console.log(couleurs[w]);
+    document.write("<p>" + couleurs[w] + "</p>");
 }
-document.write("<h3>Le mois suivant j'ai : " + mesSous + "</h3>");
-document.write("<h3>Il aura fallu " + tps + " mois.</h3>");
+
+//Affichage à partir du dernier élément
+for (var w = 2; w >= 0; w--) {
+    console.log(couleurs[w]);
+    document.write("<p>" + couleurs[w] + "</p>");
+}
