d51595e2d5ea314d2436a5ac0dfa35ae7296f328
alexis <alexissimoes94@gmail.com>
2023-06-09 16:51:14 +0200

    09/06/23

---
+++ initiation/!ex_perso/js/exo_day5.js
@@ -0,0 +1,22 @@
+// var nombre1 = 5;
+// var nombre2 = 3;
+// var nombre3;
+// console.log(nombre1);
+// console.log(nombre2);
+
+// nombre3 = nombre1;
+// nombre1 = nombre2;
+// nombre2 = nombre3;
+
+// console.log(nombre1);
+// console.log(nombre2);
+
+var age = prompt("quelle et votre age ?", "19");
+while (age <=3 || age >= 120 || isNaN(age)){
+    age = prompt("quelle et votre age ?", "19");
+}
+var majoriteFR = 18;
+function afficheAge (number){
+    
+}
+afficheAge(age);
