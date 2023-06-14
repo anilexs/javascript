4d34b35dc1249bf7f5667d30f9a649c97c358397
alexis <alexissimoes94@gmail.com>
2023-06-14 14:01:00 +0200

    14/06/23

---
+++ initiation/!ex_perso/js/exo_day7.js
@@ -0,0 +1,20 @@
+// var beta1 = ['alexis','marinet','adrient','chloe','nino','allia','rose','jouleca','zoe','luka','chat noir','ladybug'];
+// var beta2 = ['belle','gentie','attentionné','amoureu','romantique','enervent','enthousiasme','fatigue','calme','enrager','adorable','kawaii'];
+
+// function rend(nam, adj){
+//     for (var i=0; i<beta1.length; i++){
+//         var rendom = Math.floor(Math.random() * 12);
+//         var rendom2 = Math.floor(Math.random() * 12);
+//         document.write(beta1[rendom]+ " et " +beta2[rendom2] +"<br>");
+//     }
+// }
+// // rend(beta1, beta2);
+// var red = Math.random();
+// document.write(red);
+function fillarray(num){
+    var addElement = [];
+    var nb1;
+    for (var i = 0; i<=12; i++){
+        nb1 = prompt();
+    }
+}
